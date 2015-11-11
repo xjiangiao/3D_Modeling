480e268f43cc1e8f73c3ccbb41c2f0511196baa0
xjiangiao <xjtc19910103@gmail.com>
2015-11-11 17:47:49 -0500

    Update Basic.h

--- Basic.h
+++ Basic.h
@@ -9,10 +9,10 @@ private:
 public:
 	Vector3D(double x=0.0,double y=0.0 ,double z=0.0):x(x),y(y),z(z){}
 	Vector3D operator+(const Vevctor3D &lhs, const Vector3D &rhs) const{
-		return Vector3D(lhs.x+rhs.x,lhs.y+rhs.y,lhs.z+rhs.z)
+		return Vector3D(x+rhs.x,y+rhs.y,z+rhs.z)
 	}
 	Vector3D operator-(const Vevctor3D &lhs, const Vector3D &rhs) const{
-		return Vector3D(lhs.x-rhs.x,lhs.y-rhs.y,lhs.z-rhs.z)
+		return Vector3D(x-rhs.x,y-rhs.y,z-rhs.z)
 	}
 	Vector3D operator*(double s) const{
 		return Vector3D(x*s,y*s,z*s)
