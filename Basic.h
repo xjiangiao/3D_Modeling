3f2f84ca9e280124c2a92d9052682be283b208d2
xjiangiao <xjtc19910103@gmail.com>
2015-11-11 14:42:36 -0500

    Update Basic.h

--- Basic.h
+++ Basic.h
@@ -1,10 +1,14 @@
 #ifndef BASIC_H
 #define BAISC_H
 
-#include<iostream>
+#include <iostream>
+//#include <vector>
+using namespace std;
 class Framework {
 private:
+	double x,y,z;
+//	int r,g,b;
 public:
-
+	Coordinate(double x=0.0,double y=0.0 ,double z=0.0):x(x),y(y),z(z){}
 };
 #endif
