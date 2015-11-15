4784145133c8e990180cfd95d6d7a61f4242eeb9
xjiangiao <xjtc19910103@gmail.com>
2015-11-15 12:59:06 -0500

    Create cube.h

---
+++ cube.h
@@ -0,0 +1,20 @@
+#idndef CUBE_H
+#DEFINE CUBE_H
+#include <iostream>
+#inlcude "Shape3D.h"
+
+class cube : public Shape3D{
+private:
+	double x,y,z;
+private:
+	cube();
+	cube(const cube& orig);
+	~cube();
+	cube& operator= (const cube& orig);
+	cube& operator= (const cube&& orig);
+	double volume() const;
+	double area() const;
+	void roatate(int angle);
+	void translate();
+	void scale(int times);
+}
