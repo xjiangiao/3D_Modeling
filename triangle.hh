3de29bb3709aa9c01b46c0dc6be9b7ec0a221be7
joey4duncan <jduncan@stevens.edu>
2015-11-15 14:02:38 -0500

    Update triangle.hh

--- triangle.hh
+++ triangle.hh
@@ -6,10 +6,10 @@
 
 class Triangle : public 2Dshape{
 private:
-    double x1, x2, x3, y1, y2, y3;
+    double x1, x2, x3, y1, y2, y3, z1, z2, z3;
 public:
-    Triangle(double x1, double y1, double x2, double y2, dobule x3, double y3)
-        : x1(x1), x2(x2), x3(x3), y1(y1), y2(y2), y3(y3){}
+    Triangle(double x1, double y1, double z1, double x2, double y2, double z2, dobule x3, double y3 double z3)
+        : x1(x1), x2(x2), x3(x3), y1(y1), y2(y2), y3(y3), z1(z1), z2(z2), z3(z3){}
     double area() const;
     double perimeter() const;
 	void draw(int color) const;	//draw a triangle in a 2D coordinate system
