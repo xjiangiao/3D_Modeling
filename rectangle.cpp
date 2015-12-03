8c3bffd60b2a9aa06da6928f235df5a21799fbea
joey4duncan <jduncan@stevens.edu>
2015-12-02 20:58:31 -0500

    Create rectangle.cpp
    adding rectangle.cpp with draw function to output stl file

---
+++ rectangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <istream>
+//#include"2Dshape.hh"
+#include "rectangle.hh"
+using namespace std;
+
+void Rectangle::Draw() const{
+    ofstream fout("rectangle.stl");
+    fout << "solid OpenSCAD_Model" << '\n';
+    fout << "   facet normal 0 0 0" << '\n';
+    fout << "       outer loop" << '\n';
+    fout << "           vertex " << x1 << " " << y1 << " " << z1 << '\n';
+    fout << "           vertex " << x2 << " " << y2 << " " << z2 << '\n';
+    fout << "           vertex " << x3 << " " << y3 << " " << z3 << '\n';
+    fout << "       endloop" << '\n';
+    fout << "   endfacet" << '\n';
+    fout << "endsolid OpenSCAD_Model" << '\n';
+}
+
+int main(){
+    Rectangle a(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0);
+    a.draw();
+    system("pause");
+    return 0;
+}
