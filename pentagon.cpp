dcbab8c9e69b3d331868c005b3d16243a571619e
joey4duncan <jduncan@stevens.edu>
2015-12-03 23:04:39 -0500

    update to output proper stl format

--- pentagon.cpp
+++ pentagon.cpp
@@ -13,6 +13,18 @@ void Pentagon::draw() const{
 	fout << "			vertex " << x1 << " " << y1 << " " << z1 << '\n';
 	fout << "			vertex " << x2 << " " << y2 << " " << z2 << '\n';
 	fout << "			vertex " << x3 << " " << y3 << " " << z3 << '\n';
+	fout << " 		endloop" << '\n';
+	fout << "	endfacet" << '\n';
+	fout << "	facet normal 0 0 0" << '\n';
+	fout << "		outer loop" << '\n';
+	fout << "			vertex " << x1 << " " << y1 << " " << z1 << '\n';
+	fout << "			vertex " << x4 << " " << y4 << " " << z4 << '\n';
+	fout << "			vertex " << x3 << " " << y3 << " " << z3 << '\n';
+	fout << " 		endloop" << '\n';
+	fout << "	endfacet" << '\n';
+	fout << "	facet normal 0 0 0" << '\n';
+	fout << "		outer loop" << '\n';
+	fout << "			vertex " << x1 << " " << y1 << " " << z1 << '\n';
 	fout << "			vertex " << x4 << " " << y4 << " " << z4 << '\n';
 	fout << "			vertex " << x5 << " " << y5 << " " << z5 << '\n';
 	fout << " 		endloop" << '\n';
@@ -21,7 +33,11 @@ void Pentagon::draw() const{
 }
 
 int main(){
-	Pentagon a(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+	Pentagon a(3.0, 4.0, 0.0,	//x1 y1 z1
+				 5.0, 2.0, 0.0,	//x2 y2 z2
+				  4.0, 0.0, 0.0,
+				   2.0, 0.0, 0.0,
+				    1.0, 2.0, 0.0);
 	a.draw();
 	system("pause");
 	return 0;
