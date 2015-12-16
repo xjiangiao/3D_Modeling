51cb62bbdf1442f9ede58a1fcb3842cf323f9e5a
xjiangiao <xjtc19910103@gmail.com>
2015-12-16 10:44:58 -0500

    Update rectangle.hh

--- rectangle.hh
+++ rectangle.hh
@@ -11,10 +11,10 @@ public:
 		double y4, double z4) : x1(x1), x2(x2), x3(x3), x4(x4), y1(y1), y2(y2), y3(y3), y4(y4), z1(z1), z2(z2), z3(z3), z4(z4){}
 	double area() const;
 	double perimeter() const;
+	void zoom(double times, Rectangle& orig);
 	void draw() const;	//draw a circle in a 2D coordinate system
-	void rotate(double degree, Triangle& orig);
-//	void moveleft(int n);	//move the rectangle n units to the right
-//	void moveright(int n);	//move rectangle n unite to the left
+	void rotate(double degree, Rectangle& orig);
+    void move(double n, char direction, Rectangle& orig);	//move the rectangle n units to the right
 };
 
 #endif
