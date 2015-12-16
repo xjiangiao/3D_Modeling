c8d0cce3932e8c2912d4e6ee1cea9913710f3e8f
xjiangiao <xjtc19910103@gmail.com>
2015-12-16 13:18:53 -0500

    Update tetrahedron.hh

--- tetrahedron.hh
+++ tetrahedron.hh
@@ -1,26 +1,20 @@
-#ifndef TETRAHEDRON_HH_ 
-#define	TETRAHEDRON_HH_
-#include "3Dshape.hh"
+#ifndef Tetrahedron_HH_
+#define Tetrahedron_HH_
+//#include "2Dshape.hh"
 #include <iostream>
-using namespace std;
 
-class Tetrahedron : public 3Dshape {
+class Tetrahedron {
 private:
 	double x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4;
 public:
-	Tetrahedron(double x1, double y1, double z1, double x2, double y2, double z2,
-					double x3, double y3, double z3, double x4, double y4, double z4)
-				: x1(x1), x2(x2), x3(x3), x4(x4), y1(y1), y2(y2), y3(y3), y4(y4), z1(z1), z2(z2), z3(z3) {}
-	Tetrahedron(const Tetrahedron& orig);
-	~Tetrahedron();
-	Tetrahedron& operator =(const Tetrahedron& orig);
-	Tetrahedron& operator =(const Tetrahedron&& orig);
+	Tetrahedron(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3, double x4,
+		double y4, double z4) : x1(x1), x2(x2), x3(x3), x4(x4), y1(y1), y2(y2), y3(y3), y4(y4), z1(z1), z2(z2), z3(z3), z4(z4) {}
 	double area() const;
-	double volume() const;
-	void drawTetrahedron(int color) const;
-	void rotate(double ang) const;
-	void scale(int n);
-	void translate(double x, double y, double z);
+	double perimeter() const;
+	void zoom(double times, Tetrahedron& orig);
+	void draw() const;	//draw a Tetrahedron in a 2D coordinate system
+	void rotate(double degree, Tetrahedron& orig);
+	void move(double n, char direction, Tetrahedron& orig);	//move the Tetrahedron n units to the right
 };
 
 #endif
